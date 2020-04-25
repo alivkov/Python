a21067e12858eb58c802fc41c1e584d6244239ec
sa66u <alex_ivkov@yahoo.com>
2020-04-25 17:24:34 +0100

    Git init

---
+++ Basics/basics.py
@@ -0,0 +1,4 @@
+import datetime
+x = datetime.datetime.now()
+print("The date and time is:", x)
+print(type(x))
---
+++ Basics/basics_conditionals.py
@@ -0,0 +1,6 @@
+def foo(value):
+    if value = isinstance(value, str) and len(value) >= 8:
+        return(print("True"))
+    else 
+        return(print("False"))
+
