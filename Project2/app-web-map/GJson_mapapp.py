574afd4237a6b8b26f945aac7ac181217aefd5e8
sa66u <alex_ivkov@yahoo.com>
2020-06-20 16:01:55 +0100

    Final Folium Map

--- Project2/app-web-map/GJson_mapapp.py
+++ Project2/app-web-map/GJson_mapapp.py
@@ -22,14 +22,14 @@ Height: %s ft
 
 map = folium.Map([41.694410, -114.361793], zoom_start=5, tiles='Stamen Terrain')
 
-fg = folium.FeatureGroup(name='Markers')
+fgv = folium.FeatureGroup(name='Markers')
 
 for lt, ln, el, name in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=folium.Popup(iframe), fill=True, color='grey', fill_color = color_producer(el), fill_opacity=1.0))
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=10, popup=folium.Popup(iframe), fill=True, color='grey', fill_color = color_producer(el), fill_opacity=1.0))
 
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read()))
+map.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), name='Polygons', style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000 else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
 
-map.add_child(fg)
-
-map.save('map_html_GeoJson.html')
+map.add_child(fgv)
+map.add_child(folium.LayerControl())
+map.save('map_html_GeoJson_colour.html')
